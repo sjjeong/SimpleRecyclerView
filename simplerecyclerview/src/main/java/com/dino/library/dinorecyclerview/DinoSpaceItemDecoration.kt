8b3fe0c84a65571082aaccdc8b8b36316fff2d66
sjjeong <sjjeong1225@gmail.com>
2020-02-20 00:42:16 +0900

    Fix dino_itemSpace bug

--- simplerecyclerview/src/main/java/com/dino/library/dinorecyclerview/DinoSpaceItemDecoration.kt
+++ simplerecyclerview/src/main/java/com/dino/library/dinorecyclerview/DinoSpaceItemDecoration.kt
@@ -7,7 +7,9 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 
-internal class DinoSpaceItemDecoration(val space: Int) : RecyclerView.ItemDecoration() {
+internal class DinoSpaceItemDecoration(private val space: Int) : RecyclerView.ItemDecoration() {
+
+    private val halfSpace = space / 2
 
     override fun getItemOffsets(
         outRect: Rect,
@@ -42,32 +44,50 @@ internal class DinoSpaceItemDecoration(val space: Int) : RecyclerView.ItemDecora
     ) {
         val spanCount = layoutManager.spanCount
 
-        val endLineSpace =
-            if (position / spanCount == (count - 1) / spanCount) {
-                0
-            } else {
-                space
-            }
+
+        val startLineSpace = if (position / spanCount == 0) {
+            0
+        } else {
+            halfSpace
+        }
+
+        val endLineSpace = if (position / spanCount == (count - 1) / spanCount) {
+            0
+        } else {
+            halfSpace
+        }
 
         when (position % spanCount) {
+            0 -> {
+                // start position
+                when (layoutManager.orientation) {
+                    RecyclerView.HORIZONTAL -> {
+                        outRect.set(startLineSpace, 0, endLineSpace, halfSpace)
+                    }
+                    RecyclerView.VERTICAL -> {
+                        outRect.set(0, startLineSpace, halfSpace, endLineSpace)
+                    }
+                }
+            }
             spanCount - 1 -> {
                 // end position
                 when (layoutManager.orientation) {
                     RecyclerView.HORIZONTAL -> {
-                        outRect.set(0, 0, endLineSpace, 0)
+                        outRect.set(startLineSpace, halfSpace, endLineSpace, 0)
                     }
                     RecyclerView.VERTICAL -> {
-                        outRect.set(0, 0, 0, endLineSpace)
+                        outRect.set(halfSpace, startLineSpace, 0, endLineSpace)
                     }
                 }
             }
             else -> {
+                // middle position
                 when (layoutManager.orientation) {
                     RecyclerView.HORIZONTAL -> {
-                        outRect.set(0, 0, endLineSpace, space)
+                        outRect.set(startLineSpace, halfSpace, endLineSpace, halfSpace)
                     }
                     RecyclerView.VERTICAL -> {
-                        outRect.set(0, 0, space, endLineSpace)
+                        outRect.set(halfSpace, startLineSpace, halfSpace, endLineSpace)
                     }
                 }
             }
@@ -85,32 +105,50 @@ internal class DinoSpaceItemDecoration(val space: Int) : RecyclerView.ItemDecora
         val spanIndex =
             (view.layoutParams as StaggeredGridLayoutManager.LayoutParams).spanIndex
 
-        val endLineSpace =
-            if (position / spanCount == (count - 1) / spanCount) {
-                0
-            } else {
-                space
-            }
+        val startLineSpace = if (position / spanCount == 0) {
+            0
+        } else {
+            halfSpace
+        }
+
+        val endLineSpace = if (position / spanCount == (count - 1) / spanCount) {
+            0
+        } else {
+            halfSpace
+        }
+
 
         when (spanIndex) {
+            0 -> {
+                // start position
+                when (layoutManager.orientation) {
+                    RecyclerView.HORIZONTAL -> {
+                        outRect.set(startLineSpace, 0, endLineSpace, halfSpace)
+                    }
+                    RecyclerView.VERTICAL -> {
+                        outRect.set(0, startLineSpace, halfSpace, endLineSpace)
+                    }
+                }
+            }
             spanCount - 1 -> {
                 // end position
                 when (layoutManager.orientation) {
                     RecyclerView.HORIZONTAL -> {
-                        outRect.set(0, 0, endLineSpace, 0)
+                        outRect.set(startLineSpace, halfSpace, endLineSpace, 0)
                     }
                     RecyclerView.VERTICAL -> {
-                        outRect.set(0, 0, 0, endLineSpace)
+                        outRect.set(halfSpace, startLineSpace, 0, endLineSpace)
                     }
                 }
             }
             else -> {
+                // middle position
                 when (layoutManager.orientation) {
                     RecyclerView.HORIZONTAL -> {
-                        outRect.set(0, 0, endLineSpace, space)
+                        outRect.set(startLineSpace, halfSpace, endLineSpace, halfSpace)
                     }
                     RecyclerView.VERTICAL -> {
-                        outRect.set(0, 0, space, endLineSpace)
+                        outRect.set(halfSpace, startLineSpace, halfSpace, endLineSpace)
                     }
                 }
             }
