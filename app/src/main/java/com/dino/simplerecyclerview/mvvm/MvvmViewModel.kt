8820e151269f0b15d36c6f5e57ce46b7a0fbabd9
sjjeong <sjjeong1225@gmail.com>
2020-02-21 02:05:14 +0900

    add header, footer sample

--- app/src/main/java/com/dino/simplerecyclerview/mvvm/MvvmViewModel.kt
+++ app/src/main/java/com/dino/simplerecyclerview/mvvm/MvvmViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.ViewModel
 import com.dino.simplerecyclerview.ItemClickListener
 import com.dino.simplerecyclerview.model.BaseVectorAssetItem
 import com.dino.simplerecyclerview.model.VectorAsset
+import kotlin.random.Random
 
 class MvvmViewModel : ViewModel(), ItemClickListener {
 
     val vectorAssetItems = ObservableField<List<BaseVectorAssetItem>>()
 
     val selectedItem = ObservableField<BaseVectorAssetItem>()
+
+    val randomNumer = ObservableField<Long>(0)
 
     init {
         createNewItem()
@@ -22,6 +25,7 @@ class MvvmViewModel : ViewModel(), ItemClickListener {
                 .map { it.toItem() }
                 .toMutableList()
                 .shuffled())
+        randomNumer.set(Random.nextLong(1000))
     }
 
     override fun onClick(item: BaseVectorAssetItem) {
